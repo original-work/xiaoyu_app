bf0c91bda4ba14beb236cb6acfb19d20f0c633f9
winer632 <winer632@qq.com>
2017-01-18 16:34:32 +0800

    test

--- src/com/ainemo/demo/MyApplication.java
+++ src/com/ainemo/demo/MyApplication.java
@@ -11,8 +11,8 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Settings settings = new Settings("0b5d7ed54bee16756a7579c6718ab01e3d1b75eb", true, false);  // develop 鐜
-//        Settings settings = new Settings("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3", false, false);  // production 鐜
+//        Settings settings = new Settings("0b5d7ed54bee16756a7579c6718ab01e3d1b75eb", true, false);  // develop 鐜
+        Settings settings = new Settings("646d65e39c55460da902ff92b7ca2d17e2f57b03", false, false);  // production 鐜
 
         NemoSDK.getInstance().init(getApplicationContext(), settings);
     }
